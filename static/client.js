ebddf6232c0972d698bbc34565fc23de9d51ea66
Andres Rios <rianby64@gmail.com>
2018-09-02 10:54:26 +0200

    Defina el combobox para seleccionar proyecto

--- static/client.js
+++ static/client.js
@@ -1,20 +1,34 @@
 'use strict';
 ((io) => {
   var client = io();
+  var ProjectId = location.search.match(/\d+$/);
   client.on('connect', () => {
     console.log('connection');
 
     client.emit('data', {
       query: 'select',
-      module: 'viewAAUCosts1MonthFlow',
-      project: '2'
+      module: 'Projects'
     });
+
+    if (ProjectId) {
+      client.emit('data', {
+        query: 'select',
+        module: 'viewAAUCosts1MonthFlow',
+        project: ProjectId
+      });
+    }
 
   });
 
   client.on('response', (data) => {
     var query = data.query;
-    qtakeoffcostsflow.doselect(data.row);
+    if (query == 'select' && data.module == 'viewAAUCosts1MonthFlow') {
+      qtakeoffcostsflow.doselect(data.row);
+    } else if (query == 'select' && data.module == 'Projects') {
+      window.projects.doselect(data.row);
+    } else {
+      console.log('not processed', data);
+    }
   });
   window.client = client;
 })(io);
