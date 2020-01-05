83ec8cd88479380ff972e2a2e5b793a3b4067c64
Novikov Anton <anton.novikov.y@gmail.com>
2020-01-05 16:34:56 +0300

    Add $ symbol to cost

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -3,5 +3,5 @@ export function getPointOnTimeline(timeline: Array<Date>, date: Date) {
 }
 
 export function parseToDotsFormat(value: string) {
-  return value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
+  return `$${value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')}`;
 }
