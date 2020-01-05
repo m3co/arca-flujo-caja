6e7874403663df9ee1b181a979f8789ac22f7de5
Novikov Anton <anton.novikov.y@gmail.com>
2020-01-06 01:40:33 +0300

    Change color for select

--- src/components/ProjectSelect/ProjectSelect.tsx
+++ src/components/ProjectSelect/ProjectSelect.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import './ProjectSelect.less';
 
 interface ProjectSelectProps {
   currentProject: number,
